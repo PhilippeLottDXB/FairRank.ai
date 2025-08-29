4bbee55d27474a36552998c2f61eea36f1fa2337
PhilippeLottDXB <63391406+PhilippeLottDXB@users.noreply.github.com>
2025-08-29 21:24:41 +0400

    Update GPT_link.py

--- GPT_link.py
+++ GPT_link.py
@@ -1,9 +1,10 @@
 from openai import OpenAI
+import streamlit as st
 import openai
 
 # gets API Key from environment variable OPENAI_API_KEY
 def request(prompt,model = "gpt-4"):
-    key = "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"
+    key =OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
     client = OpenAI(api_key=key)
     # key = "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"
@@ -25,3 +26,4 @@ def request(prompt,model = "gpt-4"):
 if __name__ == "__main__":
     data = request("What are the top cities in europe")
     print(data)
+
