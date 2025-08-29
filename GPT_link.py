91413ca585395da5ee2b07d33258ad58e335d0a1
PhilippeLottDXB <63391406+PhilippeLottDXB@users.noreply.github.com>
2025-08-29 21:24:52 +0400

    Update GPT_link.py

--- GPT_link.py
+++ GPT_link.py
@@ -7,8 +7,6 @@ def request(prompt,model = "gpt-4"):
     key =OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
     client = OpenAI(api_key=key)
-    # key = "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"
-
     openai.api_key = key
     # Non-streaming:
     #print("----- standard request -----")
@@ -26,4 +24,5 @@ def request(prompt,model = "gpt-4"):
 if __name__ == "__main__":
     data = request("What are the top cities in europe")
     print(data)
+
 
